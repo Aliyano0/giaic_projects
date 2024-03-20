1c571eb453d1ef852c803272f85c5c0294dd3e74
Aliyano0 <aliyanaqeel97@gmail.com>
2024-03-20 06:37:36 +0500

    Added chalk

--- GIAIC_Projects/project01_number_guessing-game/index.ts
+++ GIAIC_Projects/project01_number_guessing-game/index.ts
@@ -10,13 +10,13 @@ let userInput = await inquirer.prompt([{message: "Guess the Number between 1 to 
 
 for (let score = 1; randomNumber === userInput.userInput; score++){
   if(randomNumber === userInput.userInput){
-    console.log((chalk.bgGreen)`Yayyy, you guessed the correct number: ${randomNumber}`);
+    console.log((chalk.bgGreen)`Yayyy, you guessed the correct number:${randomNumber}`);
     console.log(`Your current score is: ${score}`);
   }
   userInput = await inquirer.prompt([{message:"You are doing great! Guess another number.", type: "number", name: "userInput"}]);
-}
+};
 
 if(randomNumber !== userInput){
-  console.log((chalk.bgRed)`Wrong answer. The correct number was: ${randomNumber}.`);
-}
+  console.log((chalk.bgRed)`Wrong answer. The correct number was:${randomNumber}.`);
+};
 
