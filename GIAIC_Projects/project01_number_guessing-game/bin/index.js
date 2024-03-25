21f57fc41479dd30ee23398535818dea2dea8e64
Aliyano0 <aliyanaqeel97@gmail.com>
2024-03-25 06:18:04 +0500

    Fixed some minor mistakes

--- GIAIC_Projects/project01_number_guessing-game/bin/index.js
+++ GIAIC_Projects/project01_number_guessing-game/bin/index.js
@@ -1,17 +1,17 @@
 #! /usr/bin/env node
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-let userInput = await inquirer.prompt([{ message: "Guess the Number between 1 to 10:", type: "number", name: "userInput" }]);
-for (let score = 1; randomNumber === userInput.userInput; score++) {
-    if (randomNumber === userInput.userInput) {
-        console.log((chalk.bgGreen) `Yayyy, you guessed the correct number:${randomNumber}`);
+const randomNumber = Math.floor(Math.random() * 5) + 1;
+let userInput = await inquirer.prompt([{ message: "Guess the Number between 1 to 5:", type: "number", name: "answer" }]);
+for (let score = 1; randomNumber === userInput.answer; score++) {
+    if (randomNumber === userInput.answer) {
+        console.log((chalk.bgGreen) `Yayyy, you guessed the correct number: ${randomNumber}`);
         console.log(`Your current score is: ${score}`);
     }
-    userInput = await inquirer.prompt([{ message: "You are doing great! Guess another number.", type: "number", name: "userInput" }]);
+    userInput = await inquirer.prompt([{ message: "You are doing great! Guess another number.", type: "number", name: "answer" }]);
 }
 ;
-if (randomNumber !== userInput) {
-    console.log((chalk.bgRed) `Wrong answer. The correct number was:${randomNumber}.`);
+if (randomNumber !== userInput.answer) {
+    console.log((chalk.bgRed) `Wrong answer. The correct number was:${randomNumber}`);
 }
 ;
